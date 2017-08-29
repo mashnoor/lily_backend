966aa09a511a7b261bf89688879dd35d6929d382
Nowfel Mashnoor <nmmashnoor@gmail.com>
2017-08-29 16:24:19 +0600

    Written script for user login

--- app/Http/Controllers/UsercustomerController.php
+++ app/Http/Controllers/UsercustomerController.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\UserCustomer;
 
 class UsercustomerController extends Controller
 {
-    function test()
+    function singup(Request $request)
     {
-        return "Hello";
+        $name = $request->get('name');
+        $phone = $request->get('phone');
+        $picture = $request->get('picture');
+        $date = $request->get('date');
+        $shareCode = $request->get('shareCode');
+        $firstRide = $request->get('firstRide');
+        $token = $request->get('token');
+        $userCustomer = new UserCustomer();
+        $userCustomer->name = $name;
+        $userCustomer->phone = $phone;
+        $userCustomer->picture = $picture;
+        $userCustomer->date = $date;
+        $userCustomer->shareCode = $shareCode;
+        $userCustomer->firstRide = $firstRide;
+        $userCustomer->token = $token;
+        return response()->json([
+            'result'=>'success',
+        ]);
     }
 }
---
+++ app/UserCustomer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class UserCustomer extends Model
+{
+    protected $table = 'usercustomer';
+
+}
