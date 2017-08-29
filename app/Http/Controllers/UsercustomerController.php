8dda701f2fb5c148fb31e544443643eecb072a11
Nowfel Mashnoor <nmmashnoor@gmail.com>
2017-08-29 16:07:40 +0600

    Fixing

---
+++ app/Http/Controllers/UsercustomerController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsercustomerController extends Controller
+{
+    function test()
+    {
+        return "Hello";
+    }
+}
