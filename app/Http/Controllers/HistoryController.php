3c412f40f0a941f741a2f01e7385667ca243b3db
Nowfel Mashnoor <nmmashnoor@gmail.com>
2017-09-27 22:41:25 +0600

    Updated get history:

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\History;
 use App\UserCustomer;
+use App\UserRider;
 use Illuminate\Http\Request;
 
 class HistoryController extends Controller
@@ -12,17 +13,30 @@ class HistoryController extends Controller
     {
         $token = $request->get('token');
         $userCustomer = UserCustomer::where('token', $token)->first();
-        if(is_null($userCustomer))
+        $userRIder= UserRider::where('token', $token)->first();
+        if(is_null($userCustomer) && is_null($userRIder))
         {
             return response()->json([
-               "result" => "user not found"
+               "response" => "user not found"
+            ]);
+        }
+        else if(is_null($userRIder))
+        {
+            $userId = $userCustomer->id;
+            $histories = History::where('userCustomer_id', $userId)->get();
+            return response()->json([
+               "response"=>"customer found successfully",
+                "histories"=>$histories
             ]);
         }
         else
         {
-            $userId = $userCustomer->id;
-            $histories = History::where('userCustomer_id', $userId)->get();
-            return $histories;
+            $userId = $userRIder->id;
+            $histories = History::where('userRider_id', $userId)->get();
+            return response()->json([
+                "response"=>"rider found successfully",
+                "histories"=>$histories
+            ]);
         }
 
     }
