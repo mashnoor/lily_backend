7548f0e7f7cf24477bab9c727c09220c5d2715b6
Nowfel Mashnoor <nmmashnoor@gmail.com>
2017-11-23 19:46:27 +0600

    Added money due in riders

--- resources/views/userriders.blade.php
+++ resources/views/userriders.blade.php
@@ -6,41 +6,45 @@
     <h3>Riders</h3>
 
 
-    <div class ="form-group">
-<h4>Search</h4><form action="{{ url('/riders/search') }}" method="get">
-<input type="text" class ="form-control" id="search" placeholder="Search by User id, Name, Phone, Email or Share Code" name ="search"/>
-<br>
-   <button type="submit" class="btn btn-info">
-      <span class="glyphicon glyphicon-search"></span> Search
-    </button>
-</form>
-<br>
+    <div class="form-group">
+        <h4>Search</h4>
+        <form action="{{ url('/riders/search') }}" method="get">
+            <input type="text" class="form-control" id="search"
+                   placeholder="Search by User id, Name, Phone, Email or Share Code" name="search"/>
+            <br>
+            <button type="submit" class="btn btn-info">
+                <span class="glyphicon glyphicon-search"></span> Search
+            </button>
+        </form>
+        <br>
 
 
-    <div class="table-responsive">
-        <table class="table table-bordered table-striped">
-            <tr>
-                <th>User ID</th>
-                <th>Name</th>
-                <th>Phone</th>
-                <th>E-mail</th>
-                
-            </tr>
-            @foreach($UserRiders as $user)
+        <div class="table-responsive">
+            <table class="table table-bordered table-striped">
                 <tr>
-                    <td>{{ $user->id }}</td>
-                   <td><a href = "/riders/profile/{{$user->id}}">{{ $user->name }}</a></td>  
-                  
-                    <td>{{ $user->phone }}</td>
-                    <td>{{ $user->email }}</td>
+                    <th>User ID</th>
+                    <th>Name</th>
+                    <th>Phone</th>
+                    <th>E-mail</th>
+                    <th>Money Due</th>
 
                 </tr>
+                @foreach($UserRiders as $user)
+                    <tr>
+                        <td>{{ $user->id }}</td>
+                        <td><a href="/riders/profile/{{$user->id}}">{{ $user->name }}</a></td>
 
-            @endforeach
+                        <td>{{ $user->phone }}</td>
+                        <td>{{ $user->email }}</td>
+
+                        <td>{{ $user->moneyDue }}</td>
+                    </tr>
+
+                @endforeach
 
 
-        </table>
+            </table>
 
-    </div>
+        </div>
 
 @endsection
